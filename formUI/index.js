a9a4db8006ef8a211528d6b084ee522aefbef848
talucre <talucreee@gmail.com>
2025-10-29 20:53:58 +0300

    first part of form UI

---
+++ formUI/index.js
@@ -0,0 +1,22 @@
+function toggleShowPassword(id) {
+  const elem = document.getElementById(id);
+  const type = elem.getAttribute("type");
+  if (type === "password") {
+    elem.setAttribute("type", "text");
+  } else {
+    elem.setAttribute("type", "password");
+  }
+}
+
+const showPasswordCheckbox = document.getElementById("show-password-checkbox");
+const showConfirmPasswordCheckbox = document.getElementById(
+  "show-confirm-password-checkbox"
+);
+
+showPasswordCheckbox.addEventListener("click", () =>
+  toggleShowPassword("password")
+);
+
+showConfirmPasswordCheckbox.addEventListener("click", () =>
+  toggleShowPassword("confirm-password")
+);
