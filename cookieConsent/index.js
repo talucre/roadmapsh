fdf620a44770d499c3a8797914871a6ecea52859
talucre <talucreee@gmail.com>
2025-11-04 21:22:17 +0300

    cookieConsent

---
+++ cookieConsent/index.js
@@ -0,0 +1,23 @@
+const cookie = document.getElementById("cookie");
+const closeBtn = document.getElementById("cookie__close-btn");
+const acceptBtn = document.getElementById("cookie__accept-btn");
+
+let interval;
+
+if (!localStorage.getItem("cookie")) {
+  interval = setInterval(() => cookie.classList.add("visible"), 5000);
+}
+
+function closeCookie() {
+  cookie.classList.remove("visible");
+}
+
+closeBtn.addEventListener("click", closeCookie);
+
+function acceptCookie() {
+  localStorage.setItem("cookie", true);
+  clearInterval(interval);
+  closeCookie();
+}
+
+acceptBtn.addEventListener("click", acceptCookie);
